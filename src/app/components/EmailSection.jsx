6ea26747ba5ce0c2cc4bed25e594014c25e388a1
Aman Gupta <amanrgupta.456@gmail.com>
2024-07-11 18:09:46 +0530

    Initial Commit

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -44,12 +44,12 @@ const EmailSection = () => {
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-400 to-blue-700 opacity-30 rounded-full h-80 w-80 z-0 blur-2xl absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">
-          Let&apos;s Connect
+          Let';s Connect
         </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
-          I&apos;m currently looking for new opportunities, my inbox is always
-          open. Whether you have a question or just want to say hi, I&apos;ll
+          I';m currently looking for new opportunities, my inbox is always
+          open. Whether you have a question or just want to say hi, I';ll
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
