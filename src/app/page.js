49d2da9dfbb19ee525b08357c351344176162685
Aman Gupta <amanrgupta.456@gmail.com>
2024-07-24 20:43:20 +0530

    Add Custom cursor

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,18 @@ import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
-
+import AnimatedCursor from "react-animated-cursor";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+      <AnimatedCursor 
+      innerSize={8}
+      outerSize={12}
+      color='93, 39, 255'
+      outerAlpha={0.5}
+      innerScale={0.7}
+      outerScale={8}/>
+
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
